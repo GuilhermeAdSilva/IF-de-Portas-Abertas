090df833fe695d8f57a7d4f6667932b9459d6c79
Guilherme <guilhermeadrianodasilva69@gmail.com>
2025-10-01 20:10:10 -0300

    controle do botao

--- scripts/candidato.js
+++ scripts/candidato.js
@@ -1,3 +1,22 @@
 function mensagem() {
     window.alert("Obrigado pelo interesse! Entraremos em contato em breve!");
 }
+
+function verificarCampos() {
+        const nome = document.getElementById('nome').value;
+        const contato = document.getElementById('contato').value;
+        const dataNascimento = document.getElementById('dataNascimento').value;
+        const checkbox = document.getElementById('checkbox').checked;
+        
+        const btnEnviar = document.getElementById('btnEnviar');
+        if (nome && contato && dataNascimento && checkbox) {
+            btnEnviar.disabled = false;
+        } else {
+            btnEnviar.disabled = true;
+        }
+    }
+
+    document.getElementById('nome').addEventListener('input', verificarCampos);
+    document.getElementById('contato').addEventListener('input', verificarCampos);
+    document.getElementById('dataNascimento').addEventListener('input', verificarCampos);
+    document.getElementById('checkbox').addEventListener('change', verificarCampos);
