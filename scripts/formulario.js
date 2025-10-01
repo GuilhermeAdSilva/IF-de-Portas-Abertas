baac0ffa1b8f04c7cd5e3e0002b0e5197a6ab7e5
Guilherme <guilhermeadrianodasilva69@gmail.com>
2025-09-30 21:09:12 -0300

    v1.0.0

--- scripts/formulario.js
+++ scripts/formulario.js
@@ -4,7 +4,6 @@ const inputAnimal = document.getElementById("animal");
 const fechar = document.getElementById("fechar");
 const form = modal.querySelector("form");
 
-// Abrir modal ao clicar em "Adotar"
 botoes.forEach(botao => {
     botao.addEventListener("click", function (e) {
         e.preventDefault();
@@ -14,22 +13,19 @@ botoes.forEach(botao => {
     });
 });
 
-// Fechar modal ao clicar no X
 fechar.addEventListener("click", () => {
     modal.style.display = "none";
 });
 
-// Fechar modal ao clicar fora do conteúdo
 window.addEventListener("click", (e) => {
     if (e.target === modal) {
         modal.style.display = "none";
     }
 });
 
-// Interceptar envio do formulário
 form.addEventListener("submit", (e) => {
-    e.preventDefault(); // impede envio real
-    alert("Obrigado pelo interesse, entraremos em contato em breve!"); // mensagem para o usuário
-    modal.style.display = "none"; // fecha o modal
-    form.reset(); // limpa os campos
+    e.preventDefault();
+    alert("Obrigado pelo interesse, entraremos em contato em breve!");
+    modal.style.display = "none";
+    form.reset();
 });
